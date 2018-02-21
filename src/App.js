932a149563336f96c3d9d4a4641f3efc14baf377
Thomas Cristina de Carvalho <thomas.c.decarvalho@iskn.co>
2018-02-21 16:58:39 +0100

    Shopify

--- src/App.js
+++ src/App.js
@@ -1,44 +1,108 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {loading: false, msg: null};
-  }
-
-  handleClick = (e) => {
-    e.preventDefault();
-
-    this.setState({loading: true});
-    fetch('/.netlify/functions/hello')
-      .then(response => response.json())
-      .then(json => this.setState({loading: false, msg: json.msg}));
-  }
-
-  render() {
-    const {loading, msg} = this.state;
-
-    return <p>
-      <button onClick={this.handleClick}>{loading ? 'Loading...' : 'Call Lambda'}</button><br/>
-      <span>{msg}</span>
-    </p>
-  }
-}
+import { Link } from 'react-router-dom';
+import Products from './components/Products';
+import Cart from './components/Cart';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isCartOpen: false,
+      checkout: { lineItems: [] },
+      products: []
+    };
+
+    this.handleCartClose = this.handleCartClose.bind(this);
+    this.addVariantToCart = this.addVariantToCart.bind(this);
+    this.updateQuantityInCart = this.updateQuantityInCart.bind(this);
+    this.removeLineItemInCart = this.removeLineItemInCart.bind(this);
+  }
+
+  componentWillMount() {
+    this.props.client.collection.fetchWithProducts('Z2lkOi8vc2hvcGlmeS9Db2xsZWN0aW9uLzIzMTU0Mzg1').then((collection) => {
+      // Do something with the collection
+      this.setState({
+        products: collection.products,
+      });
+    });
+
+    this.props.client.checkout.create().then((checkout) => {
+      this.setState({
+        checkout,
+      });
+    });
+  }
+
+  addVariantToCart(variantId, quantity){
+    this.setState({
+      isCartOpen: true,
+    });
+
+    const lineItemsToAdd = [{variantId, quantity: parseInt(quantity, 10)}]
+    const checkoutId = this.state.checkout.id
+
+    return this.props.client.checkout.addLineItems(checkoutId, lineItemsToAdd).then(res => {
+      this.setState({
+        checkout: res,
+      });
+    });
+  }
+
+  updateQuantityInCart(lineItemId, quantity) {
+    const checkoutId = this.state.checkout.id
+    const lineItemsToUpdate = [{id: lineItemId, quantity: parseInt(quantity, 10)}]
+
+    return this.props.client.checkout.updateLineItems(checkoutId, lineItemsToUpdate).then(res => {
+      this.setState({
+        checkout: res,
+      });
+    });
+  }
+
+  removeLineItemInCart(lineItemId) {
+    const checkoutId = this.state.checkout.id
+
+    return this.props.client.checkout.removeLineItems(checkoutId, [lineItemId]).then(res => {
+      this.setState({
+        checkout: res,
+      });
+    });
+  }
+
+  handleCartClose() {
+    this.setState({
+      isCartOpen: false,
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to the Vivian website.</h1>
+        <header className="App__header">
+          {!this.state.isCartOpen &&
+            <div className="App__view-cart-wrapper">
+              <button className="App__view-cart" onClick={()=> this.setState({isCartOpen: true})}>Cart</button>
+            </div>
+          }
+          <div className="App__title">
+            <h1>Shopify: React Example</h1>
+          </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <LambdaDemo/>
+        <div style={{ textAlign: 'center' }}>
+          <Link to="/test"><h1>Click to test the router</h1></Link>
+        </div>
+        <Products
+          products={this.state.products}
+          addVariantToCart={this.addVariantToCart}
+        />
+        <Cart
+          checkout={this.state.checkout}
+          isCartOpen={this.state.isCartOpen}
+          handleCartClose={this.handleCartClose}
+          updateQuantityInCart={this.updateQuantityInCart}
+          removeLineItemInCart={this.removeLineItemInCart}
+        />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Test from './test';
+import Client from 'shopify-buy';
+import './app.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const client = Client.buildClient({
+  domain: 'sas-iskn.myshopify.com',
+  storefrontAccessToken: '04e570154597e38f81f17d15e81a9181',
+});
+
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route
+        exact
+        name="index"
+        path="/"
+        render={() => <App client={client}/>}
+      />
+      <Route path="/test" component={Test} />
+    </Switch>
+  </Router>,
+  document.getElementById('root')
+);
