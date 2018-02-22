77f9be252383bccd8288368ec69332adf5a32fa7
Thomas Cristina de Carvalho <thomas.c.decarvalho@iskn.co>
2018-02-22 11:57:26 +0100

    Test env variables

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,5 +1,6 @@
 export function handler(event, context, callback) {
   console.log(event)
+  console.log(process.env.REACT_APP_SECRET_CODE);
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({msg: "Hello, World!"})
